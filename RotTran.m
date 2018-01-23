4edc151074b47df3ce52a5078ac60efbc4060690
abhinavgoel95 <abhinavgoel95@gmail.com>
2018-01-23 10:28:59 -0500

    Add files via upload

---
+++ RotTran.m
@@ -0,0 +1,17 @@
+close; clear;
+a = imread('/Users/Anup/Desktop/Screen Shot 2017-01-11 at 8.41.09 AM.png');
+b = rgb2gray(a);
+r = [];
+theta = 45;
+t = [cosd(theta) -sind(theta) 30; sind(theta) cosd(theta) 30];
+t = ceil(t); %round and floor won't work
+s = size(a);
+for r_ind = 1 : s(1)
+    for c_ind = 1 : s(2)
+        x = [r_ind c_ind 1]';
+        xd = t*x;
+        r(xd(1),xd(2)) = a(r_ind, c_ind);
+    end
+end
+r = uint8(r);
+imshow(r);
